0dd70a8c76f3c64a57e4d3e5361d6053086ea784
Michael Wulber <mwulber2015@ufl.edu>
2018-11-12 20:26:38 -0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -5,10 +5,15 @@ var app = angular.module('ParkingPickerApp', ['ngMaterial']);
 app.controller('ParkingLotsController', function ($scope, $mdSidenav, $mdDialog) {
     $scope.toggleLeft = buildToggler('left');
     $scope.toggleRight = buildToggler('right');
+    $scope.decalColor = "Any";
 
     function buildToggler(componentId) {
       return function() {
         $mdSidenav(componentId).toggle();
       }
     }
+
+    $scope.setDecalColor = function(color) {
+      $scope.decalColor = color;
+    };
 });
