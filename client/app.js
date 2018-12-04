e4671fc9d6176fe73e8d8f39bd6c8ad57015ffe8
Michael Wulber <mwulber2015@ufl.edu>
2018-12-04 11:04:52 -0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -20,6 +20,53 @@ app.controller('ParkingLotsController', function ($scope, $mdSidenav, $mdDialog)
     $scope.setDecalColor = function(color) {
       $scope.decalColor = color;
     };
+    $scope.handleLoginSubmission = function(email, password) {
+      var xhr = new XMLHttpRequest();
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          console.log(xhr.responseText);
+        }
+      };
+
+      body = {
+        email: email,
+        password: password
+      };
+
+      xhr.open("POST", "https://unspecifive.herokuapp.com/api/user/login", true);
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      xhr.send(body);
+    }
+
+    $scope.handleCreateSubmission = function(name, email, username, password, decal) {
+      var xhr = new XMLHttpRequest();
+      // xhr.withCredentials = true;
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          console.log(xhr.responseText);
+          $scope.userToken = xhr.responseText;
+        }
+      };
+
+      body = {
+        email: email,
+        username: username,
+        password: password,
+        parkingDecalCode: decal,
+        name: name
+      };
+
+      var data = 'email=' + email + '&username=' + username + '&password=' + password + '&parkingDecalCode=' + decal + '&name=' + name;
+      console.log(data);
+
+      xhr.open("POST", "https://unspecifive.herokuapp.com/api/user/signup");
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+      xhr.setRequestHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      xhr.send(data);
+    }
 });
 
 
